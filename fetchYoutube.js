6b1783af965d90bbea3746b14880f61025c143dc
김태선 <kirca322@gmail.com>
2020-02-28 14:43:44 +0900

    youtube api key update

--- fetchYoutube.js
+++ fetchYoutube.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 const fetchYoutube = data => {
   let query = encodeURI(data.dataValues.item);
-  let key = 'AIzaSyAI-Ia8gNW6GASksuJCiBc5CdbrpA7e1Jk';
+  let key = 'AIzaSyAtXfgXrIOmPCr---pCtv53S2wbQwAR9Mo';
   let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${key}&q=${query}&maxResults=10&type=video&videoEmbeddable=true`;
   return fetch(url);
 };
