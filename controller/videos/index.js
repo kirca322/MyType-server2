fe04ebd2e0a83a4648c65697dd2c9708074179b8
박동건 <hbd9425@gmail.com>
2020-03-04 16:36:07 +0900

    검색API

--- controller/videos/index.js
+++ controller/videos/index.js
@@ -14,5 +14,17 @@ module.exports = {
             res.sendStatus(500);
           });
       });
+  },
+
+  searchController: (req, res) => {
+    const { searchValue } = req.body;
+    fetchYoutube(searchValue)
+      .then(res => res.json())
+
+      .then(json => res.status(200).json(json.items))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(500);
+      });
   }
 };
--- fetchYoutube.js
+++ fetchYoutube.js
@@ -1,7 +1,13 @@
 const fetch = require('node-fetch');
 const youtubeObj = require('./config/youtubeKey');
 const fetchYoutube = data => {
-  let query = encodeURI(data.dataValues.item);
+  let query;
+  if (typeof data === 'object') {
+    query = encodeURI(data.dataValues.item);
+  } else if (typeof data === 'string') {
+    query = encodeURI(data);
+  }
+  // let query = encodeURI(data.dataValues.item);
   let key = youtubeObj.secret;
   let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${key}&q=${query}&maxResults=10&type=video&videoEmbeddable=true`;
   return fetch(url);
