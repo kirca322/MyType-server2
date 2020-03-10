ad1107ab600d138a5e21a2e89b8f834883d094ec
김태선 <kirca322@gmail.com>
2020-03-10 20:28:58 +0900

    git ignore add

--- config/jwt.js
+++
@@ -1,5 +0,0 @@
-let jwtObj = {};
-
-jwtObj.secret = '@kgb';
-
-module.exports = jwtObj;
--- config/youtubeKey.js
+++
@@ -1,5 +0,0 @@
-let youtubeObj = {};
-
-youtubeObj.secret = 'AIzaSyD38NbEovvh4j73OEC-peUKRFBkbBSpW9A';
-
-module.exports = youtubeObj;
