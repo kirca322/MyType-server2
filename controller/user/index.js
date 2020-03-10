dc5c761e9aab4feec6c06a041be94dc4f8e09c9d
김태선 <kirca322@gmail.com>
2020-03-10 17:30:54 +0900

    코드 리팩토링 이전 코드 주석처리해놓은것 삭제

--- controller/user/index.js
+++ controller/user/index.js
@@ -39,41 +39,6 @@ module.exports = {
           .catch(err => {
             res.status(404).send(err);
           });
-        // 검색결과의 categories_id를 categories테이블에서 모두 찾아서
-
-        // 찾은 데이터를 응답으로 보내준다.
-
-        //
-        // users_categories
-        //   .findAll({ where: { users_id: decoded.id } })
-        //   .then(data => {
-        //     if (data) {
-        //       let checkList = []; // 유저 카테고리 리스트 번호
-        //       for (let i = 0; i < data.length; i++) {
-        //         let chk = data[i].dataValues;
-        //         checkList.push(chk.categories_id);
-        //       }
-        //       if (checkList.length === 0) {
-        //         res.status(204).json({ userCategoryList: result });
-        //       } else {
-        //         categories.findAll({ where: { id: checkList } }).then(data => {
-        //           let result = [];
-        //           for (let i = 0; i < data.length; i++) {
-        //             let chk = data[i].dataValues.category_name;
-        //             result.push(chk);
-        //           }
-        //           if (result.length > 0) {
-        //             res.status(200).json({ userCategoryList: result });
-        //           }
-        //         });
-        //       }
-        //     } else {
-        //       res.status(409).end();
-        //     }
-        //   })
-        //   .catch(err => {
-        //     res.status(404).send(err);
-        //   });
       } else {
         res.status(401).end();
       }
@@ -184,48 +149,6 @@ module.exports = {
           .catch(err => {
             res.status(404).send(err);
           });
-        // categories
-        //   .findAll({
-        //     where: {
-        //       category_name: categoryName
-        //     }
-        //   })
-        //   .then(data => {
-        //     let checkList = []; // 같은명의 카테고리 id 리스트
-        //     for (let i = 0; i < data.length; i++) {
-        //       // let chk = data[i].dataValues.id;
-        //       checkList.push(data[i].dataValues.id);
-        //     }
-        //     users_categories
-        //       .findAll({
-        //         where: {
-        //           id: checkList
-        //         }
-        //       })
-        //       .then(data => {
-        //         let checkList = []; // 같은명의 카테고리 users_id 리스트
-        //         for (let i = 0; i < data.length; i++) {
-        //           // let chk = data[i].dataValues.users_id;
-        //           checkList.push(data[i].dataValues.users_id);
-        //         }
-        //         if (checkList.indexOf(decoded.id) === -1) {
-        //           categories
-        //             .create({ category_name: categoryName })
-        //             .then(data => {
-        //               // let c = data.dataValues.id;
-
-        //               users_categories
-        //                 .create({
-        //                   categories_id: data.dataValues.id,
-        //                   users_id: decoded.id
-        //                 })
-        //                 .then(() => res.status(200).end());
-        //             });
-        //         } else {
-        //           res.status(409).end();
-        //         }
-        //       });
-        //   });
       } else {
         res.status(401).end();
       }
@@ -270,41 +193,6 @@ module.exports = {
           .catch(err => {
             res.status(404).send(err);
           });
-        // categories
-        //   .findAll({
-        //     where: { category_name: categoryName }
-        //   })
-        //   .then(data => {
-        //     if (data.length === 0) {
-        //       res.status(409).end();
-        //     } else {
-        //       let checkList = []; // 같은명의 카테고리 users_id 리스트
-        //       for (let i = 0; i < data.length; i++) {
-        //         // let chk = data[i].dataValues.id;
-        //         checkList.push(data[i].dataValues.id);
-        //       }
-
-        //       users_categories
-        //         .findAll({
-        //           where: {
-        //             categories_id: checkList
-        //           }
-        //         })
-        //         .then(data => {
-        //           let chk; // users_categories_id
-        //           for (let i = 0; i < data.length; i++) {
-        //             if (data[i].dataValues.users_id === decoded.id) {
-        //               chk = data[i].dataValues.categories_id;
-        //             }
-        //           }
-        //           users_categories_videos.create({
-        //             users_categories_id: chk,
-        //             video: JSON.stringify(video)
-        //           });
-        //           res.status(200).end();
-        //         });
-        //     }
-        //   });
       } else {
         res.status(401).end();
       }
@@ -358,50 +246,6 @@ module.exports = {
           .catch(err => {
             res.status(404).send(err);
           });
-        // categories
-        //   .findAll({
-        //     where: { category_name: categoryName }
-        //   })
-        //   .then(data => {
-        //     let checkList = []; // 같은명의 카테고리 id 리스트
-        //     for (let i = 0; i < data.length; i++) {
-        //       // let chk = data[i].dataValues.id;
-        //       checkList.push(data[i].dataValues.id);
-        //     }
-
-        //     users_categories
-        //       .findAll({
-        //         where: {
-        //           categories_id: checkList
-        //         }
-        //       })
-        //       .then(data => {
-        //         let chk; // users_categories_id
-        //         for (let i = 0; i < data.length; i++) {
-        //           if (data[i].dataValues.users_id === decoded.id) {
-        //             chk = data[i].dataValues.categories_id;
-        //           }
-        //         }
-        //         users_categories_videos
-        //           .findAll({
-        //             where: { users_categories_id: chk }
-        //           })
-        //           .then(data => {
-        //             let checkList = []; // 카테고리 video 리스트
-        //             for (let i = 0; i < data.length; i++) {
-        //               // let chk = JSON.parse(data[i].dataValues.video);
-        //               checkList.push(JSON.parse(data[i].dataValues.video));
-        //             }
-        //             if (checkList.length === 0) {
-        //               res.status(204).end();
-        //             } else {
-        //               res.status(200).json({
-        //                 videoList: checkList
-        //               });
-        //             }
-        //           });
-        //       });
-        //   });
       } else {
         res.status(401).end();
       }
@@ -443,39 +287,6 @@ module.exports = {
           .catch(err => {
             res.status(404).send(err);
           });
-        // categories
-        //   .findAll({
-        //     where: { category_name: categoryName }
-        //   })
-        //   .then(data => {
-        //     let checkList = []; // 같은명의 카테고리 users_id 리스트
-        //     for (let i = 0; i < data.length; i++) {
-        //       // let chk = data[i].dataValues.id;
-        //       checkList.push(data[i].dataValues.id);
-        //     }
-
-        //     users_categories
-        //       .findAll({
-        //         where: {
-        //           categories_id: checkList
-        //         }
-        //       })
-        //       .then(data => {
-        //         let chk; // users_categories_id
-        //         for (let i = 0; i < data.length; i++) {
-        //           if (data[i].dataValues.users_id === decoded.id) {
-        //             chk = data[i].dataValues.categories_id;
-        //           }
-        //         }
-        //         users_categories_videos.destroy({
-        //           where: {
-        //             users_categories_id: chk,
-        //             video: JSON.stringify(video)
-        //           }
-        //         });
-        //         res.status(200).end();
-        //       });
-        //   });
       } else {
         res.status(401).end();
       }
@@ -545,85 +356,6 @@ module.exports = {
           .catch(err => {
             res.status(404).send(err);
           });
-        // 카테고리에 해당하는 비디오 데이터들 삭제
-        // categories
-        //   .findAll({
-        //     where: { category_name: categoryName }
-        //   })
-        //   .then(data => {
-        //     let checkList = []; // 같은명의 카테고리 users_id 리스트
-        //     for (let i = 0; i < data.length; i++) {
-        //       // let chk = data[i].dataValues.id;
-        //       checkList.push(data[i].dataValues.id);
-        //     }
-        //     users_categories
-        //       .findAll({
-        //         where: {
-        //           categories_id: checkList
-        //         }
-        //       })
-        //       .then(data => {
-        //         let chk; // users_categories_id
-        //         for (let i = 0; i < data.length; i++) {
-        //           if (data[i].dataValues.users_id === decoded.id) {
-        //             chk = data[i].dataValues.categories_id;
-        //           }
-        //         }
-        //         users_categories_videos.destroy({
-        //           where: {
-        //             users_categories_id: chk
-        //           }
-        //         });
-        //       });
-        //   });
-        // // 카테고리 삭제
-
-        // categories
-        //   .findAll({
-        //     where: {
-        //       category_name: categoryName
-        //     }
-        //   })
-        //   .then(data => {
-        //     let checkList = []; // 같은명의 카테고리 id 리스트
-        //     for (let i = 0; i < data.length; i++) {
-        //       // let chk = data[i].dataValues.id;
-        //       checkList.push(data[i].dataValues.id);
-        //     }
-
-        //     users_categories
-        //       .findAll({
-        //         where: {
-        //           id: checkList
-        //         }
-        //       })
-        //       .then(data => {
-        //         let checkList = []; // 같은명의 카테고리 users_id 리스트
-
-        //         for (let i = 0; i < data.length; i++) {
-        //           // let chkNumber = data[i].dataValues;
-        //           checkList.push(data[i].dataValues);
-        //         }
-        //         let cid; // 카테고리아이디
-        //         for (let k = 0; k < checkList.length; k++) {
-        //           if (checkList[k].users_id === decoded.id) {
-        //             cid = checkList[k].categories_id;
-        //           }
-        //         }
-
-        //         users_categories.destroy({
-        //           where: {
-        //             categories_id: cid
-        //           }
-        //         });
-        //         categories.destroy({
-        //           where: {
-        //             id: cid
-        //           }
-        //         });
-        //       });
-        //     res.status(200).end();
-        //   });
       } else {
         res.status(401).end();
       }
